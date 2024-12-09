15dc0878f2958a8a4865741e376a1a2c4f0aa301
Datasapien <bradleyclayton89@gmail.com>
2024-12-09 21:13:47 +0000

    Create test file for make_api_get_request

---
+++ test/test_extract/test_make_api_get_request.py
@@ -0,0 +1,8 @@
+from src.extract.extract_utils import make_api_get_request
+from unittest.mock import patch
+from datetime import datetime
+import unittest
+import pytest
+
+def test_returns_expected_file_name():
+    pass
