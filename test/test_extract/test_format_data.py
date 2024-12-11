0b1f01e91c8643e32e8afa10bed074d017794a11
Datasapien <bradleyclayton89@gmail.com>
2024-12-11 21:46:54 +0000

    Add tests to format_data test suite

--- test/test_extract/test_format_data.py
+++ test/test_extract/test_format_data.py
@@ -42,12 +42,36 @@ def test_empty_data():
 
     assert result == []
 
-def test_combines_dicts_correctly():
+def test_combines_dicts_correctly_for_single_forecast():
     test_input = {'city': {'lat': 1, 'lon':2}, 
                   'list': [{'pressure': 3, 'temp':4}]}
 
     result = format_data(test_input)
 
     expected = [{'lat': 1, 'lon': 2, 'pressure': 3, 'temp': 4}]
+
+    assert result == expected
+
+def test_combines_dicts_correctly_for_multiple_forecasts():
+    test_input = {'city': {'lat': 1, 'lon': 2}, 
+                  'list': [{'pressure': 3, 'temp': 4},
+                           {'pressure': 5, 'temp': 6}]}
+
+    result = format_data(test_input)
+
+    expected = [{'lat': 1, 'lon': 2, 'pressure': 3, 'temp': 4},
+                {'lat': 1, 'lon': 2, 'pressure': 5, 'temp': 6}]
+
+    assert result == expected
+
+def test_for_more_nested_levels():
+    test_input = {'city': {'lat': 1, 'lon': 2}, 
+                  'list': [{'pressure': 3, 'temp': 4, 'clouds': {'all': 100}},
+                           {'pressure': 5, 'temp': 6, 'clouds': {'all': 100}}]}
+
+    result = format_data(test_input)
+
+    expected = [{'lat': 1, 'lon': 2, 'pressure': 3, 'temp': 4, 'clouds.all': 100},
+                {'lat': 1, 'lon': 2, 'pressure': 5, 'temp': 6, 'clouds.all': 100}]
 
     assert result == expected
