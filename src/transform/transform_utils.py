9fe2457ecd95817435ea2c7e9c4947c26c624511
Datasapien <bradleyclayton89@gmail.com>
2024-12-13 14:30:40 +0000

    WIP - transform util functions

--- src/transform/transform_utils.py
+++ src/transform/transform_utils.py
@@ -20,7 +20,7 @@ def get_data_frame(s3_client, data_bucket, key):
     Parameters:
         s3_client: S3 client
         data_bucket: name of the ingested bucket (stored in .env)
-        key: reference to the file in the S3 bucket    
+        key: reference to the file in the S3 bucket     
     """
     try:
         obj = s3_client.get_object(Bucket=data_bucket, Key=key)
@@ -30,3 +30,28 @@ def get_data_frame(s3_client, data_bucket, key):
     except Exception as e:
         print(f"Error loading DataFrame for key '{key}': {e}")
         return None
+    
+def update_data_frame():
+    pass
+
+def calculate_time_increase():
+    pass
+
+def calculate_cost_increase():
+    pass
+
+def convert_to_parquet():
+    pass
+
+def store_in_s3(s3_client, parquet_file, bucket_name, file_name):
+    """
+    Uploads parquet file to a named AWS S3 bucket.
+
+    Parameters:
+        s3_client: boto3 S3 client
+        parquet_file: transformed data in parquet format
+        bucket_name (str): S3 bucket name
+        file_name (str): S3 file directory and file name
+    """
+    
+    s3_client.put_object(Body=parquet_file, Bucket=bucket_name, Key=file_name)
