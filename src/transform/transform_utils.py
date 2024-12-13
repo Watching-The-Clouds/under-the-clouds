cf173a83cbaf440c8736a0e425faacc1b5cecaf1
Datasapien <bradleyclayton89@gmail.com>
2024-12-13 12:16:20 +0000

    Write create_s3_client and get_data_frame util functions

--- src/transform/transform_utils.py
+++ src/transform/transform_utils.py
@@ -1,7 +1,32 @@
-# https://www.geeksforgeeks.org/convert-epoch-time-to-date-time-in-python/
+import boto3
+import pandas as pd
+import io
+import os
 
-# drop unnecessary columns
-# categorise weather codes into 'light rain / heavy rain / snow / fog'
+data_bucket = os.environ.get("INGESTION_BUCKET")
 
-# apply % impacts from  https://ops.fhwa.dot.gov/weather/q1_roadimpact.htm to generate time increase to journey:
-    # reduction in speed = increase in time over a given distance (calculate per mile)
+def create_s3_client():
+    """
+    Creates an S3 client using boto3.
+
+    Returns:
+        An S3 client
+    """
+    return boto3.client("s3")
+
+def get_data_frame(s3_client, data_bucket, key):
+    """Fetches and returns a DataFrame from ingested S3 bucket.
+    
+    Parameters:
+        s3_client: S3 client
+        data_bucket: name of the ingested bucket (stored in .env)
+        key: reference to the file in the S3 bucket    
+    """
+    try:
+        obj = s3_client.get_object(Bucket=data_bucket, Key=key)
+        file_stream = io.StringIO(obj['Body'].read().decode('utf-8'))
+        return pd.read_csv(file_stream)
+    
+    except Exception as e:
+        print(f"Error loading DataFrame for key '{key}': {e}")
+        return None
