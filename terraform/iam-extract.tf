97553f3be631d7305bad6591181abc2c7429da3a
yellowchimney <irina.redactor@gmail.com>
2024-12-17 17:47:23 +0000

    restore branch

--- terraform/iam-extract.tf
+++ terraform/iam-extract.tf
@@ -99,3 +99,5 @@ resource "aws_iam_role_policy_attachment" "extract_policy_attachment" {
     role        = aws_iam_role.extract_role.name
     policy_arn  = aws_iam_policy.extract_lambda_policy.arn 
 }
+
+#test test
