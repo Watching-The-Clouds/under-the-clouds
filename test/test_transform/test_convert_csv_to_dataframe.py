d2ac2fb1d665c51760df3137740bf6c0736affe1
Datasapien <bradleyclayton89@gmail.com>
2025-01-02 10:35:16 +0000

    Test for convert_csv_to_dataframe

---
+++ test/test_transform/test_convert_csv_to_dataframe.py
@@ -0,0 +1,20 @@
+from transform_utils import convert_csv_to_dataframe
+import pandas as pd
+import pytest
+
+def test_convert_csv_to_dataframe():
+
+    test_csv_content = "clouds,lat,lon,country\n100,1.1, 1.2,IT\n100,1.3,1.4,UK"
+
+    expected_output = pd.DataFrame({
+        "clouds": [100, 100],
+        "lat": [1.1,1.3],
+        "lon": [1.2,1.4],
+        "country": ["IT", "UK"]
+    })   
+
+    result = convert_csv_to_dataframe(test_csv_content)
+
+    # assert result == expected_output
+    pd.testing.assert_frame_equal(result, expected_output)
+
