aad6ac6b62f069490869ce5c488fe35c2007b7fb
yellowchimney <irina.redactor@gmail.com>
2025-01-15 13:53:46 +0000

    started working on API

---
+++ src/API/connect.py
@@ -0,0 +1,11 @@
+import psycopg2
+import os
+
+def connect_to_db():
+    return psycopg2.connect(
+    host= os.environ["DB_HOST"],
+    port= os.environ["DB_PORT"],
+    dbname= os.environ["DB_NAME"],
+    user= os.environ["DB_USER"],
+    password= os.environ["DB_PASSWORD"]
+    )
