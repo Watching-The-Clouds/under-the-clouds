f1d84ca989980dc5c0d909490e25bf14cdaafa4e
Datasapien <bradleyclayton89@gmail.com>
2025-01-16 14:35:46 +0000

    Test endpoints

--- src/API/app.py
+++ src/API/app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from src.API.connect import connect_to_db
-
+import psycopg
+# from psycopg2.extras import DictCursor
 
 app = FastAPI()
 
 @app.get("/healthcheck")
 def healthcheck():
     return {"status": "ok"}
-
 
 @app.get("/get_weather_impact")
 def get_weather_impact():
@@ -20,4 +20,26 @@ def get_weather_impact():
     conn.close()
     return weather_impact
 
+# Add ability for user to input values for speed and distance of journey (defaults: speed = 20 mph, distance = 1 mile)
 
+@app.get("/trimmed")
+def get_weather_impact():
+    conn = connect_to_db()
+    query = "SELECT city, date_time, feels_like, weather_description, visibility_description, speed_coefficient_low, speed_coefficient_high, fuel_usage_coefficient_low, fuel_usage_coefficient_high FROM weather_forecasts ORDER BY index DESC LIMIT 40"
+    with conn.cursor(row_factory=psycopg.rows.dict_row) as cursor:
+        cursor.execute(query)
+        weather_impact = cursor.fetchall()
+
+    conn.close()
+    return weather_impact
+
+@app.get("/trimmed2")
+def get_weather_impact():
+    conn = connect_to_db()
+    query = "SELECT index, city, date_time FROM weather_forecasts ORDER BY index ASC LIMIT 40"
+    with conn.cursor(row_factory=psycopg.rows.dict_row) as cursor:
+        cursor.execute(query)
+        weather_impact = cursor.fetchall()
+
+    conn.close()
+    return weather_impact
