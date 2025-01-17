5c096dbc94233959fc75c5a88aec25e75d2b4d94
Datasapien <bradleyclayton89@gmail.com>
2025-01-17 12:57:08 +0000

    Update to ensure unique Index values for each row based on number of rows in table

--- src/load/load_utils.py
+++ src/load/load_utils.py
@@ -61,10 +61,13 @@ def write_dataframe_to_rds(df, table_name, db_config):
     db_url = f"postgresql+psycopg2://{db_config['user']}:{db_config['password']}@{host}:{db_config['port']}/{db_config['dbname']}"
     
     logging.info(f"DB URL (without credentials): postgresql+psycopg2://user:***@{host}:{db_config['port']}/{db_config['dbname']}")
-
-    if "index" not in df.columns:
-        df = df.reset_index(drop=True)  # Reset index to ensure unique sequential values
-        df.insert(0, "index", range(1, len(df) + 1))  # Add `index` as the first column 
-    
+ 
     engine = create_engine(db_url)
+
+    with engine.connect() as connection:
+        result_object = connection.execute(f"SELECT COUNT(*) FROM {table_name}")
+        current_row_count = result_object.scalar()
+
+    df['Index'] = range(current_row_count, current_row_count + len(df))
+
     df.to_sql(table_name, engine, if_exists="append", index=False)
